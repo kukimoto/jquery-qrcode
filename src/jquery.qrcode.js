1831978f51928b319db347aa59d293a2d0c5d08a
kukimoto <kukimoto@me.com>
2020-06-08 15:25:39 +0900

    Update jquery.qrcode.js
    add ID as QRcode in option

--- src/jquery.qrcode.js
+++ src/jquery.qrcode.js
@@ -14,6 +14,7 @@
 			typeNumber	: -1,
 			correctLevel	: QRErrorCorrectLevel.H,
                         background      : "#ffffff",
+			id		:"QRCode",
                         foreground      : "#000000"
 		}, options);
 
